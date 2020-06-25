e8f9510dd8589621e8d1da87887012cc01fd3b48
georgemanlosajr <georgemanlosajr@gmail.com>
2020-06-25 18:27:07 +0800

    Added style switcher

---
+++ js/styleSwitcher.js
@@ -0,0 +1,10 @@
+
+
+function setActiveStyle(colorName){
+    $(".alternate-style").attr("disabled","true");
+    $(".alternate-style[title="+colorName+"]").removeAttr("disabled");
+}
+
+$(".toogle-style-switcher").click(function(){
+    $(".style-switcher").toggleClass("open");
+})
